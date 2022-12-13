851e1eef7a14effb5d23e3b194f1329e58b1ee65
R.Kleijnen <58695027+Rkleijnen@users.noreply.github.com>
2022-12-13 16:28:56 +0100

    Update hardware_graph.py
    Added missing license statement at the beginning of the file.

--- hardware_graph.py
+++ hardware_graph.py
@@ -1,3 +1,22 @@
+"""
+hardware_graph.py
+
+Copyright (C) 2022, R. Kleijnen, Forschungszentrum Jülich,
+Central Institute of Engineering, Electronics and Analytics—Electronic Systems (ZEA-2)
+
+This file is part of NeuCoNS.
+NeuCoNS is a free application: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+NeuCoNS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with NeuCoNS. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from math import inf, ceil, floor, exp, log2, pi, sqrt
 import sim_log
 import time
